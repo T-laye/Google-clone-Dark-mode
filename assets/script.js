1a8cac6e4c4444ed7bd9a9add65a6130b3ba3164
Laye <tlayefaah@gmail.com>
2022-08-17 18:50:15 +0100

    dark mode enabled

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,7 @@ const body = document.querySelector("body");
 // Header
 const nav_links = document.querySelector(".nav-items");
 const sign_in = document.querySelector(".sign-in");
+const dark_sign_in = document.querySelector(".sign-btn");
 const app_icon = document.querySelector(".app-icon");
 const dark_app_icon = document.querySelector(".dark-app-icon");
 
@@ -26,4 +27,44 @@ const language = document.querySelector(".langs");
 
 // Footer
 const nigeria = document.querySelector(".nigeria");
-const others = document.querySelector("others");
+const others = document.querySelector(".others");
+
+// Dark mode button
+const dark_mode_btn = document.querySelector(".mode");
+
+dark_mode_btn.addEventListener("click", () => {
+  //switch-btn
+  dark_mode_btn.classList.toggle("mode-dark");
+  if (dark_mode_btn.innerText === "Dark-Mode") {
+    dark_mode_btn.innerText = "Light-mode";
+  } else {
+    dark_mode_btn.innerText = "Dark-Mode";
+  }
+  // body
+  body.classList.toggle("dark-mode");
+  // Header
+  nav_links.classList.toggle("nav-items-dark");
+
+  dark_sign_in.classList.toggle("dark-sign");
+
+  app_icon.classList.toggle("hidden");
+  dark_app_icon.classList.toggle("hidden");
+
+  // Logo
+  logo.classList.toggle("hidden");
+  dark_logo.classList.toggle("hidden");
+
+  search.classList.toggle("search-dark");
+  search_bar.classList.toggle("search-bar-dark");
+
+  // Search buttons
+  search_btn.classList.toggle("hidden");
+  dark_btn.classList.toggle("hidden");
+
+  // Language
+  language.classList.toggle("langs-dark");
+
+  // Footer
+  nigeria.classList.toggle("nigeria-dark");
+  others.classList.toggle("others-dark");
+});
