3d327f38d0218ee6b266997f95d59e6e83c98e96
Laye <tlayefaah@gmail.com>
2022-08-17 18:26:11 +0100

    added dark-mode button

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// Body
+const body = document.querySelector("body");
+
+// Header
+const nav_links = document.querySelector(".nav-items");
+const sign_in = document.querySelector(".sign-in");
+const app_icon = document.querySelector(".app-icon");
+const dark_app_icon = document.querySelector(".dark-app-icon");
+
+// Logo
+const logo = document.querySelector(".logo");
+const dark_logo = document.querySelector(".logo-dark");
+
+// Search bar
+const search = document.querySelector(".search");
+const search_bar = document.querySelector(".search-bar");
+
+// Search btns
+const search_btn = document.querySelector(".search-btn");
+const dark_btn = document.querySelector(".btn-dark");
+
+// Lang section
+const language = document.querySelector(".langs");
+
+// Footer
+const nigeria = document.querySelector(".nigeria");
+const others = document.querySelector("others");
